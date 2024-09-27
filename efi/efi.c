dd510b4fb89dbad0a180dac529fae9c6791dc83d
winterver <295107659@qq.com>
2024-09-27 21:43:15 +0800

    compact efi

--- efi/efi.c
+++ efi/efi.c
@@ -6,16 +6,24 @@ void seek_read(FILE* f, size_t pos, void* buf, size_t n) {
     fread(buf, 1, n, f);
 }
 
-int main(int argc, char* argv[]) {
+__attribute__((naked, noreturn))
+void jump_to_kernel(Elf64_Addr entry) {
+    asm("mov %0, %%rax\n\t"
+        "jmp *%%rax\n"
+        ::"r"(entry));
+}
+
+__attribute__((noreturn))
+void load_and_enter_kernel(int argc, char* argv[]) {
     if (argc < 2) {
         printf("usage: elfload <elf_file>\n");
-        return -1;
+        exit(-1);
     }
 
     FILE* f = fopen(argv[1], "r");
     if (f == NULL) {
         printf("no such file: %s\n", argv[1]);
-        return -1;
+        exit(-1);
     }
 
     Elf64_Ehdr ehdr;
@@ -24,14 +32,14 @@ int main(int argc, char* argv[]) {
     char verify[] = { 0x7f, 'E', 'L', 'F', ELFCLASS64 };
     if (memcmp(ehdr.e_ident, verify, sizeof(verify))) {
         printf("not an ELF64 file: %s\n", argv[1]);
-        return -1;
+        exit(-1);
     }
 
     if (ehdr.e_shnum == 0) {
         printf("can't load %s: no section or "
                "too many sections (>= %d)\n",
                argv[1], SHN_LORESERVE);
-        return -1;
+        exit(-1);
     }
 
     for (int i = 1; i < ehdr.e_shnum; i++) {
@@ -47,9 +55,9 @@ int main(int argc, char* argv[]) {
 
     exit_bs();
 
-    asm("mov %0, %%rax\n\t"
-        "jmp *%%rax\n"
-        :
-        :"m"(ehdr.e_entry)
-        :"rax");
+    jump_to_kernel(ehdr.e_entry);
+}
+
+int main(int argc, char* argv[]) {
+    load_and_enter_kernel(argc, argv);
 }
